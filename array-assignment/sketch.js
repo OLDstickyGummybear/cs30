29bf954a02a4865574ff5f30e826625c81e36aae
OLDstickyGummybear <127148759+OLDstickyGummybear@users.noreply.github.com>
2023-03-10 11:47:14 -0600

    worked on project

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -1,7 +1,12 @@
-let faces = [];
+let completeFaces = [];
+
+let faceList = [];
 
 function setup() {
   createCanvas(400, 400);
+  face1 = loadImage('faceParts/baseface/face1.jpg') //placeholder
+  faceList.push(face1);
+  
 }
 
 function draw() {
@@ -10,22 +15,26 @@ function draw() {
 }
 
 function drawFaces() {
-  for (let i = 0; i < faces.length; i ++) {
-
+  for (let i = 0; i < completeFaces.length; i ++) {
+    
   }
 }
 
 function createFace() {
   for (let i = 0; i <= 16; i ++) {
     
+    let randFace = random(faceList.length - 1);
     let randEye = random(eyeList.length - 1);
+    let randNose = random(noseList.length - 1);
+    let randMouth = random(mouthList.length - 1);
+    let randBrow = random(browList.length - 1);
 
     let randomFace = {
-      face: randFace,
+      face: faceList[random(faceList.length - 1)],
       eye: randEye,
       nose: randNose,
       mouth: randMouth,
-
+      brow: randBrow
     }
 
     faces.push(randomFace);
