1ed210c7f7b91a424f14a9ea3d2d65301d1bf599
OLDstickyGummybear <kevin.wmci23@gmail.com>
2023-03-19 23:20:53 -0600

    worked

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -1,4 +1,6 @@
 
+
+
 'all placeholders' // How many of each feature there is, for populateFeatureArray()
 let numberOfFaces = 4;
 let numberOfEyes = 5;
@@ -19,79 +21,164 @@ let noseList = [];
 let mouthList = [];
 let browList = [];
 
-'could probably make into a for loop'
-let listOfCoordinates = [{x: width * 1/5, y: height * 1/5}, {x: width * 1/5, y: height * 2/5}, {x: width * 1/5, y: height * 3/5}, {x: width * 1/5, y: height * 4/5}, {x: width * 2/5, y: height * 1/5},]
+let faceTransOrigins = [];
+
+let sphereRadius = 100;
 
 // let featureLists = [faceList, eyeList, noseList, mouseList, browList];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  populateFeatureArrays(); 
-  generateFaces();
-  
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+  // populateFeatureArrays(); 
+  // populateTransOriginArray();
+  generateFaces2();
 }
+
 
 function draw() {
   background(220);
   drawFaces();
-  circle(50, 50, 45);
 }
+
+
 
 function drawFaces() {
-  
-  for (let i = 0; i < completeFaces.length; i ++) {
-    // push and pop each face's transformation matrix
+  translate(windowWidth/2 * -1, windowHeight/2 * -1);
+
+  for (let Face of completeFaces) {
+    
+    push();
+    translate(Face.originX, Face.originY, Face.z);
+    rotateY(Face.time / 5);
+
+    sphere(Face.radius);
+
+    Face.z = sin(Face.time) * 100 - 200;
+    Face.time += 5;
+    
+    pop();
+    // push();
+    // translate(Origin.x, Origin.y);
+    // pop();
   }
+    // push and pop each face's transformation matrix
+  
 }
 
-function generateFaces() {
 
-  for (let i = 0; i <= numOfCompleteFaces; i ++) {
-    
-    // selects a random feature out of a feature list
-    let randBaseFace = random(faceList.length - 1);
-    let randEye = random(eyeList.length - 1);
-    let randNose = random(noseList.length - 1);
-    let randMouth = random(mouthList.length - 1);
-    let randBrow = random(browList.length - 1);
-    
-    // plugs the feature indices into the object randomFace
-    let randomFace = {
-      face: randBaseFace,
-      eye: randEye,
-      nose: randNose,
-      mouth: randMouth,
-      brow: randBrow
-    };
+function generateFaces2() {
+  for (let xIter = 1; xIter <= 4; xIter++) {
+    for (let yIter = 1; yIter <= 4; yIter++) {
+      // selects a random feature out of a feature list
 
-    // Pushes randomFace into the array of all the randomly generated faces completeFaces
-    completeFaces.push(randomFace);
+      // let randBaseFace = random(faceList.length - 1);
+      // let randEye = random(eyeList.length - 1);
+      // let randNose = random(noseList.length - 1);
+      // let randMouth = random(mouthList.length - 1);
+      // let randBrow = random(browList.length - 1);
+      
+      // plugs the feature indices into the object randomFace
+      let randomFace = {
+        // face: randBaseFace,
+        // eye: randEye,
+        // nose: randNose,
+        // mouth: randMouth,
+        // brow: randBrow,
+
+        // set the spacial propeties for each face
+        originX: width * xIter/4 - width/8,
+        originY: height * yIter/4 - height/8,
+        z: 0,
+        radius: sphereRadius,
+        time: xIter * 30 + yIter * 30,
+      };
+      
+      // Pushes randomFace into the array of all the randomly generated faces completeFaces
+      completeFaces.push(randomFace);
+    }
   }
 }
 
 function populateFeatureArrays() {
   for (let i = 1; i <= numberOfFaces; i ++) {
-    let fileName = `faceParts/baseface/${i}.jpg`;
+    let fileName = `faceParts/baseface/face${i}.jpg`;
     faceList.push(loadImage(fileName));
   }
       
   for (let i = 1; i <= numberOfEyes; i ++) {
-    let fileName = `faceParts/eyes/${i}.jpg`;
+    let fileName = `faceParts/eyes/eye${i}.jpg`;
     eyeList.push(loadImage(fileName));
   }
 
   for (let i = 1; i <= numberOfNoses; i ++) {
-    let fileName = `faceParts/noses/${i}.jpg`;
+    let fileName = `faceParts/noses/nose${i}.jpg`;
     noseList.push(loadImage(fileName));
   }
 
   for (let i = 1; i <= numberOfMouths; i ++) {
-    let fileName = `faceParts/mouths/${i}.jpg`;
+    let fileName = `faceParts/mouths/mouth${i}.jpg`;
     mouthList.push(loadImage(fileName));
   }
 
-  for (let i = 1; i <= numberOfNoses; i ++) {
-    let fileName = `faceParts/noses/${i}.jpg`;
-    noseList.push(loadImage(fileName));
+  for (let i = 1; i <= numberOfBrows; i ++) {
+    let fileName = `faceParts/brows/brow${i}.jpg`;
+    browList.push(loadImage(fileName));
   }
 }
+
+// function drawFaces() {
+//   translate(windowWidth/2 * -1, windowHeight/2 * -1);
+//   let z = 0;
+//   for (let Origin of faceTransOrigins) {
+    
+//     push();
+//     translate(Origin.x, Origin.y, z);
+//     sphere(80);
+    
+//     pop();
+//     // push();
+//     // translate(Origin.x, Origin.y);
+//     // pop();
+//   }
+//     // push and pop each face's transformation matrix
+  
+// }
+
+// function generateFaces() {
+
+//   for (let i = 0; i <= numOfCompleteFaces; i ++) {
+    
+//     // selects a random feature out of a feature list
+//     let randBaseFace = random(faceList.length - 1);
+//     let randEye = random(eyeList.length - 1);
+//     let randNose = random(noseList.length - 1);
+//     let randMouth = random(mouthList.length - 1);
+//     let randBrow = random(browList.length - 1);
+    
+//     // plugs the feature indices into the object randomFace
+//     let randomFace = {
+//       face: randBaseFace,
+//       eye: randEye,
+//       nose: randNose,
+//       mouth: randMouth,
+//       brow: randBrow,
+//       x,
+//       y,
+//       z: 0,
+//     };
+
+//     // Pushes randomFace into the array of all the randomly generated faces completeFaces
+//     completeFaces.push(randomFace);
+//   }
+// }
+
+
+// function populateTransOriginArray() {
+//   for (let xIter = 1; xIter <= 4; xIter++) {
+//     for (let yIter = 1; yIter <= 4; yIter++) {
+//       faceTransOrigins.push({x: width * xIter/ 5, y: height * yIter/5});
+//     }
+//   }
+// }
+
