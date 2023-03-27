0bc63c5dd42971fa05cf21f02df9754f632844b5
OLDstickyGummybear <127148759+OLDstickyGummybear@users.noreply.github.com>
2023-03-27 15:21:00 -0600

    March 27th
    got it to display.
    need to figure out how to make block blcok

--- projects/2d-perlin-noise/sketch.js
+++ projects/2d-perlin-noise/sketch.js
@@ -2,37 +2,86 @@
 
 let noiseArray = [];
 
-let zoom = 100;
+let zoom = 50;
+
+let genXWidth = 100;
+let genZWidth = 100;
+
+let genFloor = 12; // y= 12 is lowest terrain
+let genCeiling = 1; // y= 50 is highest terrain
+
+let cubeWidth = 30;
+
+let camX = 0;
+let camY = 0;
+let camZ = 0;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   strokeWeight(1);
   generateNoise();
+  camera = createCamera();
 }
 
 
 function draw() {
-  for (let y = 1; y <= height; y ++) {
-    for (let x = 1; x <= width; x ++) {
-      stroke(50, Math.floor(map(noiseArray[y][x], 0, 1, 10, 255)), 100);
-      point(x, y);
+  // for (let z = 0; z < genZWidth; z ++) {
+  //   for (let x = 0; x < genXWidth; x ++) {
+  //     stroke(50, Math.floor(map(noiseArray[z][x], genFloor, genCeiling, 10, 255)), 100);
+  //     point(x, z);
+  //   }
+  // }
+  background(0);
+  renderTerrain();
+  
+  
+}
+
+function renderTerrain() {
+  translate(-300, -300, 0)
+  for (let z = 0; z < genZWidth; z ++) {
+    for (let x = 0; x < genXWidth; x ++) {
+      push();
+      translate(x * cubeWidth - 100, noiseArray[z][x] + 200, z * cubeWidth);
+      box(cubeWidth, cubeWidth, cubeWidth);
+      pop();
     }
   }
-  
 }
 
 function generateNoise() {
   // text('Loading...', 10, 20);
   // noiseArray = [];
-  for (let y = 1; y <= height; y ++) {
+  for (let z = 1; z <= genZWidth; z ++) {
     let newColumn = [];
-    for (let x = 1; x <= width; x ++) {
-      newColumn.push(noise(x / zoom, y / zoom));
-      // newColumn.push(map(noise(x / 5, y / 5) / noise(y / 4, x / 4), 0, 4, 0, 1)); 
+    for (let x = 1; x <= genXWidth; x ++) {
+      blockHeight = round(map(noise(x / zoom, z / zoom), 0, 1, 0, 500));
+      newColumn.push(blockHeight);
     }
     noiseArray.push(newColumn);
   }
+  console.log('terrain generated');
+}
 
+function keyPressed() {
+  if (keyIsDown(87)) {
+    camera.move(0, 0, -10)
+  }
+  if (keyIsDown(83)) {
+    camera.move(0, 0, 10)
+  }
+  if (keyIsDown(65)) {
+    camera.move(-10, 0, 0)
+  }
+  if (keyIsDown(68)) {
+    camera.move(10, 0, 0)
+  }
+  if (keyIsDown(32)) {
+    camera.move(0, -10, 0)
+  }
+  if (keyIsDown(16)) {
+    camera.move(0, 10, 0)
+  }
 }
 
 // function keyPressed() {
