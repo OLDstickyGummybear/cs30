6341b1f3e40b7c59effbb1232aa0fb9d5ba1d719
OLDstickyGummybear <127148759+OLDstickyGummybear@users.noreply.github.com>
2023-03-28 15:11:18 -0600

    worked

--- projects/2d-perlin-noise/sketch.js
+++ projects/2d-perlin-noise/sketch.js
@@ -1,11 +1,12 @@
 
 
-let noiseArray = [];
+let worldArray = [];
 
 let zoom = 50;
 
 let genXWidth = 100;
 let genZWidth = 100;
+let genYHeight = 50;
 
 let genFloor = 12; // y= 12 is lowest terrain
 let genCeiling = 1; // y= 50 is highest terrain
@@ -21,46 +22,66 @@ function setup() {
   strokeWeight(1);
   generateNoise();
   camera = createCamera();
+  worldArray = createEmpty3dArray(genXWidth, genYHeight, genZWidth);
 }
 
 
 function draw() {
   // for (let z = 0; z < genZWidth; z ++) {
   //   for (let x = 0; x < genXWidth; x ++) {
-  //     stroke(50, Math.floor(map(noiseArray[z][x], genFloor, genCeiling, 10, 255)), 100);
+  //     stroke(50, Math.floor(map(worldArray[z][x], genFloor, genCeiling, 10, 255)), 100);
   //     point(x, z);
   //   }
   // }
   background(0);
   renderTerrain();
-  
-  
+}
+
+function createEmpty3dArray(arrayX, arrayY, arrayZ) {
+  let volume = [];
+  for (let y = 0; y < arrayY; y++) {
+    volume.push([]);
+    for (let x = 0; x < arrayX; x++) {
+      volume[y].push([]);
+      for (let z = 0; z < arrayZ; z++) {
+        volume[y][x].push(0);
+      } 
+    }
+  }
+  return volume;
 }
 
 function renderTerrain() {
-  translate(-300, -300, 0)
-  for (let z = 0; z < genZWidth; z ++) {
-    for (let x = 0; x < genXWidth; x ++) {
-      push();
-      translate(x * cubeWidth - 100, noiseArray[z][x] + 200, z * cubeWidth);
-      box(cubeWidth, cubeWidth, cubeWidth);
-      pop();
-    }
-  }
+  // // Wrong form
+  // translate(-300, -300, 0)
+  // for (let z = 0; z < genZWidth; z ++) {
+  //   for (let x = 0; x < genXWidth; x ++) {
+  //     push();
+  //     translate(x * cubeWidth - 100, worldArray[z][x] + 100, z * cubeWidth);
+  //     box(cubeWidth, cubeWidth, cubeWidth);
+  //     pop();
+  //   }
+  // }
 }
 
 function generateNoise() {
   // text('Loading...', 10, 20);
-  // noiseArray = [];
-  for (let z = 1; z <= genZWidth; z ++) {
-    let newColumn = [];
-    for (let x = 1; x <= genXWidth; x ++) {
-      blockHeight = round(map(noise(x / zoom, z / zoom), 0, 1, 0, 500));
-      newColumn.push(blockHeight);
+  // worldArray = [];
+  // for (let z = 1; z <= genZWidth; z ++) {
+  //   let newColumn = [];
+  //   for (let x = 1; x <= genXWidth; x ++) {
+  //     blockHeight = round(map(noise(x / zoom, z / zoom), 0, 1, 0, 500));
+  //     newColumn.push(blockHeight);
+  //   }
+  //   worldArray.push(newColumn);
+  // }
+  // console.log('terrain generated');
+
+  for (let x = 0; x < worldArray[0].length; x++) {
+    for (let z = 0; z < worldArray[0][0].length; z++) {
+      worldArray[round(map(noise(x / zoom, z / zoom), 0, 1, 0, genYHeight))][x][z] = 1;
     }
-    noiseArray.push(newColumn);
   }
-  console.log('terrain generated');
 }
 
 function keyPressed() {
